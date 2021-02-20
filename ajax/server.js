59cd242b655e29ca230ec6d4b046c397d61d415c
Giselle Quarto <gisellequarto@gmail.com>
2021-02-20 22:24:20 +0000

    ajax jquery

--- ajax/server.js
+++ ajax/server.js
@@ -44,7 +44,7 @@ app.get('/parOuImpar', (req, res) => {
     // req.body
     // req.query
     // req.params
-    const par = parseInt(req.query.numero) % 2 === 0
+    const par = parseInt(req.query.numero) % 2 === 0;
     res.send({
         resultado: par ? 'par' : 'impar'
     })
