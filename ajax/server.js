b5ab20ec48934ce5dbf7e1c756605bcfcea14754
Giselle Quarto <gisellequarto@gmail.com>
2021-02-20 14:32:14 +0000

    ajax part 2

---
+++ ajax/server.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/teste', (req, resp) => resp.send('OK! Servidor está on!'));
+
+
+app.listen(8080, () => console.log('Executando...'));
