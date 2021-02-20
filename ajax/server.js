13eb7aa1a950a34bba6af11562566e6acf7c0b12
Giselle Quarto <gisellequarto@gmail.com>
2021-02-20 18:14:52 +0000

    ajax part 5

--- ajax/server.js
+++ ajax/server.js
@@ -7,20 +7,30 @@ app.use(express.static('.'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const multer = require('multer')
+const multer = require('multer');
 
 /* salvar arquivos e garantir que eles terão nomes diferentes */
 const storage = multer.diskStorage({
     destination: function(req, file, callback) {
-        callback(null, './upload')
+        callback(null, './upload');
     },
     filename: function(req, file, callback) {
         /* concatenar a data com o nome do arquivo original */
-        callback(null, `${Date.now()}_${file.originalname}`)
+        callback(null, `${Date.now()}_${file.originalname}`);
     }
 })
 
-const upload = multer({ storage }).single('arquivo')
+const upload = multer({ storage }).single('arquivo');
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
 
 app.get('/teste', (req, resp) => resp.send('OK! Servidor está on!'));
 
