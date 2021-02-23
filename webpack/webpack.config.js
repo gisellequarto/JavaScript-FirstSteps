18ad8d172426b750038625cf7eaccec7a54ad83f
Giselle Quarto <gisellequarto@gmail.com>
2021-02-23 20:58:17 +0000

    webpack css

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,8 +3,19 @@ const webpack = require('webpack');
 module.exports = {
     mode: 'development',
     entry: './src/principal.js',
-     output: {
+    output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                //MiniCssExtractPlugin.loader,
+                'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader' // interpreta @import, url()...
+                //'sass-loader',
+            ]
+        }]
+    }
 }
