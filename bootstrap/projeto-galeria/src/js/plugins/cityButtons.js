9974c33450cfe59b954d544d03d457ddff36ed9b
Giselle Quarto <gisellequarto@gmail.com>
2021-03-01 18:16:17 +0000

    projeto galeria buttons 2

--- bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
+++ bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -15,3 +15,19 @@ function filterByCity(city) {
 }
 
 const cityButtons = $('[wm-city-buttons]');
+const cities = new Set;
+
+$('[wm-city]').each(function(i,e) {
+    cities.add($(e).attr('wm-city'));
+})
+
+const btns = Array.from(cities).map(city => {
+     
+    const btn = $('<button>').addClass(['btn', 'btn-info']).html(city)
+     
+    btn.click(e => filterByCity(city));
+
+    return btn;
+})
+
+const
