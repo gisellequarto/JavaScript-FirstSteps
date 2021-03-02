8afbf8fc7f5af12b61a287ae8706338ee55463df
Giselle Quarto <gisellequarto@gmail.com>
2021-03-02 15:39:23 +0000

    react basic 3

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 
 //componentes personalizados precisam SEMPRE ter letra maiuscula
 //import Primeiro from './componentes/primeiro'
-import BomDia from './componentes/bomDia'
+//import BomDia from './componentes/bomDia'
+//import { BoaTarde, BoaNoite } from './componentes/multiplos'
 
-ReactDOM.render(<BomDia nome="Maria" idade={3}/>,  document.getElementById('root'));
+import Multi from './componentes/multiplos'
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <Multi.BoaNoite nome="Bia" />
+    </div>
+    , document.getElementById('root'));
 
