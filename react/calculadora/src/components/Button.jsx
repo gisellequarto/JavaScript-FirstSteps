c1c15b86e1c7c26d13add2f278abd49aea006d62
Giselle Quarto <gisellequarto@gmail.com>
2021-03-04 19:00:57 +0000

    project calculator react button

---
+++ react/calculadora/src/components/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+const Button = props =>
+    <button className="button">{props.label}</button>
+
+
+export default Button;
--- react/calculadora/src/index.js
+++ react/calculadora/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
-
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
