59fac788d827aa71b282bd606fdd7a4b986bb4c8
Giselle Quarto <gisellequarto@gmail.com>
2021-03-05 19:49:25 +0000

    crud react nav css

--- react/crud/frontend/src/components/template/Nav.jsx
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 
 const nav = props => {
     return <aside className="menu-area">
-              Navegação
+              <nav className="menu">
+                <a href="#/">
+                  <i className="fa fa-home"></i> Início
+                </a>
+                <a href="#/users">
+                  <i className="fa fa-users"></i> Usuários
+                </a>
+              </nav>
             </aside>   
 }
 
