3ea7d3702f8744a57c75fec12c0dd71cd7ec443b
Giselle Quarto <gisellequarto@gmail.com>
2021-03-05 19:33:46 +0000

    crud react header css

--- react/crud/frontend/src/components/template/Header.jsx
+++ react/crud/frontend/src/components/template/Header.jsx
@@ -2,8 +2,11 @@ import './Header.css';
 import React from 'react';
 
 const header = props => {
-    return <header className="header">
-             Cabeçalho
+    return <header className="header d-none d-sm-flex flex-column">
+                <h1 className="mt-3">
+                    <i className={`fa fa-${props.icon}`}></i> {props.title}
+                </h1>
+                <p className="lead text-muted">{props.subtitle}</p>
             </header>
 }
 
--- react/crud/frontend/src/components/template/Main.jsx
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -4,7 +4,7 @@ import Header from './Header';
 
 const main = props => {
     return <React.Fragment>
-                <Header />
+                <Header {...props}/>
                 <main className="content">
                     Conteúdo
                 </main>
