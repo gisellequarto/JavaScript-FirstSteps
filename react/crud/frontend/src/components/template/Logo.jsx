cfcdd91fdff8e34eb93b29ed2b6059aed71678ce
Giselle Quarto <gisellequarto@gmail.com>
2021-03-05 19:24:11 +0000

    projeto crud logo css

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,10 +1,14 @@
 import './Logo.css';
+import logo from '../../assets/imgs/logo.png';
 import React from 'react';
 
-const logo = props => {
+
+const logotipo = props => {
     return <aside className="logo">
-                Logo
+                <a href="/" className="logo">
+                    <img src={logo} alt="logo"/>
+                </a>
             </aside>
 }
 
-export default logo;
+export default logotipo;
