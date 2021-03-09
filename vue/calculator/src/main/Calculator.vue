a7336ed06342586787816e51b9765a963369c736
Giselle Quarto <gisellequarto@gmail.com>
2021-03-09 08:06:15 +0000

    vue componentes

---
+++ vue/calculator/src/main/Calculator.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="calculator">
+    <Display />
+    <Button />
+</div>
+</template>
+
+<script>
+import Display from "../components/Display";
+import Button from "../components/Button";
+
+export default {
+  components: { Button, Display },
+};
+</script>
+
+<style>
+</style>
