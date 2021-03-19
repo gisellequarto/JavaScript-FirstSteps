a21ba8f663cd224acdd1524c11a5484f046011de
Giselle Quarto <gisellequarto@gmail.com>
2021-03-19 10:00:45 +0000

    express 3

--- exercicios-Express/index.js
+++ exercicios-Express/index.js
@@ -2,6 +2,37 @@ const express = require('express')
 const app = express()
 //const bodyParser = require('body-parser')
 
+app.use((req, res, next) => {
+    console.log('Antes...')
+    next()
+})
+
+app.get('/opa', (req, res, next) => {
+    console.log('Durante...')
+    res.json({
+        data: [
+            {id: 7, name: 'Ana', position: 1 },
+            {id: 34, name: 'Bia', position: 2 },
+            {id: 73, name: 'Carlos', position: 3 }
+        ],
+        count: 30,
+        skip: 0,
+        limit: 3,
+        status: 200
+    })
+
+    next()
+    
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+
+    // res.send('<h1>Estou bem!</h1><br><br><h2>Tipo é HTML!</h2>')
+})
+
+
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
