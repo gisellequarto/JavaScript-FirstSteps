e79fe0d4509a24b5301759f4524df130bbea7d77
Giselle Quarto <gisellequarto@gmail.com>
2021-03-19 10:43:05 +0000

    dados da requisição

--- exercicios-Express/saudacaoMid.js
+++ exercicios-Express/saudacaoMid.js
@@ -1,8 +1,8 @@
 function saudacao(nome) {
 
     return function(req, res, next) {
-        console.log(`Seja bem vindo ${nome}.`)    
-        next()
+        console.log(`Seja bem vindo ${nome}.`);
+        next();
     }
 }
 
